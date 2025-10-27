5c033ca42bfa7701ee4d9ba6cc3c516ef5c3353e
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2025-10-27 09:13:00 +0000

    data types

---
+++ o1_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+let name = "Prakhar"
+let id = 23
+
+//number
+//bigInt
+//string
+//boolean = "true/false"
+//null = standalone value
+//undefined
+//symbol => unique
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined);  //undefined
+
+
