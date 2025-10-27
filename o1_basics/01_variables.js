e5c504c7891334b67a0dbeb259a705c113290621
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2025-10-27 08:52:20 +0000

    container config done

---
+++ o1_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 12345
+let accountEmail = "prakharmish@gmail.com"
+var accountPassword = "234512345"
+accountCity = "Kanpur"
+let accountState
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
