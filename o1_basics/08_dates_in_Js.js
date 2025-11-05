296f15bac4d3cd9767cf98df87ff52d991e9fe0b
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2025-11-05 17:33:52 +0000

    date in JavaScript

---
+++ o1_basics/08_dates_in_Js.js
@@ -0,0 +1,34 @@
+let newDate = new Date();
+console.log(newDate.getHours());
+
+// console.log(newDate);//complex to understand
+// //converting to string
+// console.log(newDate.toString());//more readable
+
+
+// console.log(newDate.toDateString());
+// console.log(newDate.toISOString());
+// console.log(newDate.toJSON());
+// console.log(newDate.toLocaleDateString());
+// console.log(newDate.toLocaleString());
+
+// let myCreatedDate = new Date("2023-10-12");
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(newDate.getTime());//to convert the date into total ms since Epoch time
+// to convert into secs divide by 1000
+
+// console.log(Math.floor(Date.now()/1000));// to get the current time in seconds
+// console.log(`Current day is ${newDate.getDay()}`);
+// console.log(`Current hour is ${newDate.getHours()}`);
+// console.log(`Current month is ${newDate.getMonth()}`);
+// console.log(`Current year is ${newDate.getFullYear()}`);
+
+
+
+
+
+
+
