629aa7abd54bb13a33e3701d25f21d35168df179
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2025-11-05 14:42:32 +0000

    string functions

---
+++ o1_basics/05_string.js
@@ -0,0 +1,28 @@
+const newName = new String("Prakhar");
+//creates a string object in the heap and value 'Prakhar' in string pool
+//in heap, newName in stack as a reference to created string object in heap
+// console.log(newName);
+// console.log((newName.__proto__));
+// console.log(newName.charAt(3));
+// console.log(newName.length);
+// console.log(newName.indexOf('t'));
+
+// const stringName = "Hello-Prakhar-Mishra"
+
+// //at()
+// const sentence = "The quick brown fox jumps over the lazy dog.";
+
+// let index = 6;
+
+// console.log(`An index of ${index} from ${sentence} return the char ${sentence.at(index)}`); // => i
+// index = -6
+// console.log(`An index of ${index} from ${sentence} return the char ${sentence.at(index)}`);//starts the count
+// //from backward => y
+// console.log(sentence.at(-200));
+
+console.log("Well".concat(" ","hello"," ","World"));
+
+
+
+
+
