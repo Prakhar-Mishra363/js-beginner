0ea28d6f75c9b7c7dad6f46f8fcaaf9390c39e55
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2025-11-08 03:31:44 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,34 @@
+const myArr = [0,1,2,3,4,'Prakhar']
+// console.log(myArr['5']);//valid => Prakhar
+
+// const arr = new Array("hello",1,2,3.4,"Unknown")
+
+// console.log(typeof myArr);
+/*
+                            no diff b/w these two statements
+console.log(typeof arr);*/
+
+//Array methods
+// 1.Push(element) => adds the element at the end of the array
+// myArr.push(6);
+// console.log(myArr);// => [ 0, 1, 2, 3, 4, 'Prakhar', 6 ]
+
+//2. pop() => simply removes the lst element from the array
+
+// myArr.pop()
+// console.log(myArr); // =>  [ 0, 1, 2, 3, 4 ]
+
+//3. unshift(element) => adds the element at the start of the array and shifts all other element 1 position to the right
+// myArr.unshift(9)
+// console.log(myArr); // => [ 9, 0, 1, 2, 3, 4, 'Prakhar' ]
+
+
+//4. shift() => removes the first element from the array and shifts all other element 1 position to the left
+
+// myArr.shift();
+// console.log(myArr); // => [ 1, 2, 3, 4, 'Prakhar' ]
+
+
+
+
+
